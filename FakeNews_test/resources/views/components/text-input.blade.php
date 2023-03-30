8036d05e2bcb0bef0d27ba4acd2f7447eb620d67
MehmetAkifGoge <83518568+MAkif2004@users.noreply.github.com>
2023-03-30 13:36:12 +0200

    de FakeNews website met Laravel

--- FakeNews_test/resources/views/components/text-input.blade.php
+++
@@ -1,3 +0,0 @@
-@props(['disabled' => false])
-
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-300 text-input dark:border-gray-700 dark:text-black-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm']) !!}>
