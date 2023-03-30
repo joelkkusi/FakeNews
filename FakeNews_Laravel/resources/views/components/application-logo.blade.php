ca4e6f08c7fd90b238a903c97aed4c0656073b39
MehmetAkifGoge <83518568+MAkif2004@users.noreply.github.com>
2023-03-30 13:35:34 +0200

    de FakeNews website met Laravel

---
+++ FakeNews_Laravel/resources/views/components/application-logo.blade.php
@@ -0,0 +1,1 @@
+{{-- <img id="logo" src="/images/L_Logo.png" alt="Fake" width="150"> --}}
---
+++ FakeNews_Laravel/resources/views/posts/all.blade.php
@@ -0,0 +1,11 @@
+<x-layout>
+    <div id="news-body" class="news-body">
+        <div class="container">
+            <article>
+                <header>
+
+                </header>
+            </article>
+        </div>
+    </div>
+</x-layout>
